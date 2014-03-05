e4a8ebf5e0daeca27a438dda416ed787c8de4867
TamiresMariaSSS <tachatinha@hotmail.com>
2014-03-05 18:36:50 -0300

    Classes e Metodos

--- imobiliaria.py
+++ imobiliaria.py
@@ -1,8 +1,10 @@
 #coding:utf-8
 
 class Imovel(object):
+    imoveis_vendidos = []
+    imoveis_comprados = []
 
-   def __init__(self, bairro, area, descricao, endereco, proprietario, tipo_imovel, preco_compra):
+    def __init__(self, bairro, area, descricao, endereco, proprietario, tipo_imovel, preco_compra):
         self.bairro = bairro
         self.area = area
         self.descricao = descricao
@@ -13,7 +15,40 @@ class Imovel(object):
         self._preco_minimo = preco_compra * 1.10
         self._preco_venda = 0
         self.novo_proprietario = None
-        self.antigo_proprietario = None
+        self.antigo_proprietario = None 
+    
+    def __eq__(self, imovel2):
+        return self.endereco == imovel2.endereco and self.bairro == imovel2.bairro
+
+    def comprar(self, antigo_proprietario):
+        if self not in Imovel.imoveis_comprados:
+            self.antigo_proprietario = antigo_proprietario            
+            Imovel.imoveis_comprados.append(self)
+        else:
+            raise ValueError
+
+    def vender(self, preco_de_venda, novo_proprietario):
+        if self in Imovel.imoveis_comprados:
+            if preco_de_venda >= self._preco_minimo:
+                self._preco_venda = preco_de_venda
+                self.novo_proprietario = novo_proprietario
+                Imovel.imoveis_vendidos.append(self)
+                Imovel.imoveis_comprados.remove(self)
+            else:
+                raise ValueError("preco de venda >= self_preco minimo")
+        else:
+            raise ValueError("nao esta na lista comprados")
+
+    def listar_imoveis_disponiveis(self):
+        return Imovel.imoveis_comprados
+    
+    def listar_imoveis_vendidos(self, bairro):
+        lista_imoveis_vendidos = []
+        for i, imovel in enumerate(Imovel.imoveis_vendidos):
+            if imovel.bairro == bairro:
+                lista_imoveis_vendidos.append("%s %s %s %.0f %.0f" %(imovel.bairro, imovel.antigo_proprietario, imovel.novo_proprietario, imovel._preco_venda, imovel._preco_compra))
+
+        return lista_imoveis_vendidos
         
 class Proprietario(object):
     lista_proprietarios = []
@@ -29,4 +64,29 @@ class Proprietario(object):
             Proprietario.lista_proprietarios.append(self)
         else:
             raise ValueError("Error no Cadastro de Proprietário!")
+
+    def listar_proprietarios_compradores(self):
+        contador = 0
+        lista = []
+        for imovel in Imovel.imoveis_vendidos:
+            for proprietario in Proprietario.lista_proprietarios:
+                if imovel.novo_proprietario == proprietario:
+                    contador += 1
+            if contador > 1:
+                if proprietario not in lista:           
+                    lista.append(proprietario)
+        
+        return lista
+
+    def listar_proprietarios_vendedores(self):
+        contador = 0
+        lista = []
+        for imovel in Imovel.imoveis_comprados:
+            for proprietario in Proprietario.lista_proprietarios:
+                if imovel.antigo_proprietario == proprietario:
+                    contador += 1
+                if contador >1:
+                    if proprietario not in lista:
+                        lista.append(proprietario)
+        return lista
 
--- spec/imobiliaria_spec.py
+++ spec/imobiliaria_spec.py
@@ -20,12 +20,57 @@ class TestImovel(unittest.TestCase):
         imovel._preco_venda |should| equal_to(0)
         imovel.novo_proprietario |should| equal_to(None)
         imovel.antigo_proprietario |should| equal_to(None)
-        
-        
-class TestProprietario(unittest.TestCase):
 
+    def teste_compra_imovel(self):
+
+        Imovel.imoveis_comprados = []
+        Imovel.imoveis_vendidos = []
+
+        imovel = Imovel('bairro', 100, 'descricao', 'endereco', 'proprietario', 'tipo_imovel', 5.000)
+        proprietarioAntigo = Proprietario('Marciano', 123456, 'rua qualquer', 654321)
+        imovel.comprar(proprietarioAntigo)
+        imovel.imoveis_comprados[0] |should| equal_to(imovel)
+
+        imovel2 = Imovel('bairro1', 1020, 'descricao', 'endereco1', 'proprietario', 'tipo_imovel', 7.000)
+        proprietarioAntigo2 = Proprietario('Cleito', 123456, 'rua qualquer', 654321)
+        imovel2.comprar(proprietarioAntigo2)
+        imovel2.imoveis_comprados[1] |should| equal_to(imovel2)
+
+    def teste_imoveis_disponiveis(self):
+        Imovel.imoveis_comprados = []
+        Imovel.imoveis_vendidos = []
+        imovel = Imovel('bairro', 100, 'descricao', 'endereco', 'proprietario', 'tipo_imovel', 5.000)
+        proprietarioAntigo = Proprietario('Cleito', 123456, 'rua qualquer', 654321)
+        imovel.comprar(proprietarioAntigo)
+        
+        imovel2 = Imovel('bairro1', 100, 'descricao', 'endereco1', 'proprietario', 'tipo_imovel', 5.000)
+        proprietarioAntigo2 = Proprietario('Cleito', 123456, 'rua qualquer', 654321)        
+        imovel2.comprar(proprietarioAntigo2)
+        imovel.listar_imoveis_disponiveis() |should| equal_to([imovel, imovel2])
+	
+    def teste_venda_imovel(self):
+        Imovel.imoveis_comprados = []
+        Imovel.imoveis_vendidos = []
+        imovel = Imovel('bairro', 100, 'descricao', 'endereco', 'proprietario', 'tipo_imovel', 5.000)
+        proprietarioAntigo = Proprietario('Marciano', 123456, 'rua qualquer', 654321)
+        imovel.comprar(proprietarioAntigo)
+        proprietarioNovo = Proprietario('José', 123456, 'rua qualquer', 654321) 
+        imovel.vender(6000, proprietarioNovo)
+        Imovel.imoveis_vendidos[0] |should| equal_to(imovel)
+
+    def teste_listar_imoveis_vendidos(self):
+        Imovel.imoveis_comprados = []
+        Imovel.imoveis_vendidos = []
+        imovel = Imovel('bairro', 100, 'descricao', 'endereco', 'proprietario', 'tipo_imovel', 5000)
+        imovel.comprar("proprietarioAntigo")
+        imovel.vender(50000, "proprietarioNovo")
+        lista = []
+        lista.append(imovel)
+        imovel.listar_imoveis_vendidos('bairro') |should| equal_to(["bairro proprietarioAntigo proprietarioNovo 50000 5000"])
+
+
+class TestProprietario(unittest.TestCase):
     def teste_cria_objeto_proprietario(self):
-    
         proprietario=Proprietario('nome', '123.456.789-0', 'endereco', 'telefone')
         proprietario.nome |should| equal_to('nome')
         proprietario.cpf |should| equal_to('123.456.789-0')
@@ -33,14 +78,12 @@ class TestProprietario(unittest.TestCase):
         proprietario.telefone |should| equal_to('telefone')
 
     def teste_cadastro_proprietarios(self):
-    
         proprietario=Proprietario('nome', '123.456.789-0', 'endereco', 'telefone')
         proprietario.cadastrar_proprietario() 
         proprietario.lista_proprietarios[0] |should| equal_to(proprietario)
 
 
     def teste_lista_proprietarios(self):
-    
         lista_proprietarios = []
         imoveis_vendidos = []
         imoveis_comprados = []
@@ -50,4 +93,29 @@ class TestProprietario(unittest.TestCase):
         proprietario2.cadastrar_proprietario()
         imovel=Imovel('bairro', 100, 'descricao', 'endereco', proprietario1, 'tipo_imovel', 5.000)
         imovel2=Imovel('bairro2', 103, 'descricao2', 'endereco2', proprietario1, 'tipo_imovel', 6.000)
+    
+        imovel.comprar(proprietario1)
+        imovel2.comprar(proprietario1)
+        
+        imovel.vender(50000,proprietario2)
+        imovel2.vender(30000,proprietario2)
+        proprietario1.listar_proprietarios_compradores() |should| equal_to([proprietario2])
+
+
+    def teste_lista_proprietarios_vendedores(self):
+        lista_proprietarios = []
+        imoveis_vendidos = []
+        imoveis_comprados = []
+        proprietario1=Proprietario('nome', '123.456.789-0', 'endereco', 'telefone')
+       
+        proprietario1.cadastrar_proprietario()
+       
+        imovel=Imovel('bairro', 100, 'descricao', 'endereco', proprietario1, 'tipo_imovel', 5.000)
+        imovel2=Imovel('bairro2', 103, 'descricao2', 'endereco2', proprietario1, 'tipo_imovel', 6.000)
+    
+        imovel.comprar(proprietario1)
+        imovel2.comprar(proprietario1)
+
+        proprietario1.listar_proprietarios_vendedores() |should| equal_to([proprietario1])
+        
     
